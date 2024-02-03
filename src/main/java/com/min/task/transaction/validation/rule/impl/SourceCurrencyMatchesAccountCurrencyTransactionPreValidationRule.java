f16d2ac5da0748a69f1e8653d3561477d113b8f2
Vitalijs <vitalijs.bmi@gmail.com>
2024-02-03 23:50:58 +0200

    feat(MIN-9): The currency of funds in the transfer operation must match the receiver's account currency

---
+++ src/main/java/com/min/task/transaction/validation/rule/impl/SourceCurrencyMatchesAccountCurrencyTransactionPreValidationRule.java
@@ -0,0 +1,35 @@
+package com.min.task.transaction.validation.rule.impl;
+
+import com.min.task.account.repository.AccountRepository;
+import com.min.task.exception.AccountIdDoesNotExistException;
+import com.min.task.transaction.dto.TransactionRequest;
+import com.min.task.transaction.validation.rule.TransactionRequestPreValidationRule;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Order(2)
+@Slf4j
+public class SourceCurrencyMatchesAccountCurrencyTransactionPreValidationRule implements TransactionRequestPreValidationRule {
+
+    private final AccountRepository accountRepository;
+
+
+    @Override
+    public void validate(TransactionRequest request) {
+        accountRepository.findDtoById(request.sourceAccountId())
+                .ifPresent(account -> {
+                            if (!account.currency().equalsIgnoreCase(request.sourceCurrency())) {
+                                log.info("Source currency does not match source account currency");
+                                throw new AccountIdDoesNotExistException(
+                                        "Source currency does not match source account currency");
+                            }
+                        }
+
+                );
+
+    }
+}
